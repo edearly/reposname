a13bdd25f1617c74f168fd4146efe388bd699822
edearly <33229746+edearly@users.noreply.github.com>
2017-11-25 20:07:25 -0500

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author erinearly
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException 
+     {
+         
+    ServerSocket servSoc = new ServerSocket(4445);
+    Socket soc = servSoc.accept();
+    Scanner scan = new Scanner(soc.getInputStream());
+         int number = scan.nextInt();
+         
+         while (number =! 0 )
+         {
+         int temp = number * 2;
+         PrintStream ps = new PrintStream(soc.getOutputStream());
+         ps.println(temp);
+         System.out.print("Message to server recieved");
+         }
+    
+     }
+    /*
+    public static void main(String[] args) throws IOException 
+     {
+    ServerSocket servSoc = new ServerSocket(4445);
+    Socket soc = servSoc.accept();
+    Scanner scan = new Scanner(soc.getInputStream());
+         String message = scan.next();
+         while(true)
+         {
+         String temp = message;
+         PrintStream ps = new PrintStream(soc.getOutputStream());
+         ps.println(temp);
+         System.out.print("Message to server recieved");
+         }
+    
+     }
+*/
+}
+    
+    
+
