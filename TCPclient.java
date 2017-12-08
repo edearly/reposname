45036e1ba3aa6437aa49b740cce6008e9c1bb714
edearly <33229746+edearly@users.noreply.github.com>
2017-12-08 02:15:18 -0500

    Add files via upload

--- TCPclient.java
+++ TCPclient.java
@@ -9,7 +9,6 @@ package tcpclient;
  *
  * @author erinearly
  */
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -20,9 +19,12 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
 
-class TCPclient{  
+public class TCPclient{  
+    
 public static void main(String args[])throws Exception{  
-String address = "";  //definging a empty string to add the IP address
+    
+    
+String address = "";  //defining a empty string to add the IP address
 Scanner scanner=new Scanner(System.in);  //accepts input from user 
 System.out.println("Enter Server Address: ");  //debugging, control which address to use
 address=scanner.nextLine(); //passing user input into empty string
@@ -34,28 +36,39 @@ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  //
 System.out.println("start server...");
 String s="",filename = "";  // be able to choose a file on your pc
 try{  //
-while(!s.equals("start"))
-	s = br.readLine(); 
+while(s.equals("start")) //starting the server
+	s = br.readLine(); //reading user input
  
 	dataOut.writeUTF(s); //write information to the output stream
 	dataOut.flush();  
 	
-	filename = dataIn.readUTF(); //reading input and put into empty string 
+	filename = dataIn.readUTF(); //reading data from file and put into empty string 
 	System.out.println("Receving file: "+filename); //debugging 
-	filename="client"+filename;
+	filename="New"+filename; //new name for file
 	System.out.println("Saving as file: "+filename); //debugging 
 //
-long sz=Long.parseLong(dataIn.readUTF());
-System.out.println ("File Size: "+(sz/(1024*1024))+" MB");
+long size =Long.parseLong(dataIn.readUTF()); //declaring variable for file size
+     // string to int
+System.out.println ("File Size: "+ size +" MB"); //printing File size
+  //indicate file content is saving or saved 
 
-byte b[]=new byte [1024]; //defining the size 
-System.out.println("Receving file.."); //debugging 
+byte b[]=new byte [1024]; //standard size?
+System.out.println("Saving file.."); //debugging 
 FileOutputStream fileOut = new FileOutputStream(new File(filename),true);
+long receivedBytes;
+do
+{
+    receivedBytes = dataIn.read(b, 0, b.length); //bytes in file
+    fileOut.write(b,0, b.length); //writing/saving all bytes to file 
+    
+}
+while (!(receivedBytes < 1024)); //all bytes received in file
+   System.out.println("File Saved"); //debugging
 }
 catch(EOFException e)
 {
-	
+    
 }
 }
-}
+} 
     
